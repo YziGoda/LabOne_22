dbd1bf697435350e61d32fefcaa7f7c731467406
YziGoda <yzigoda@gmail.com>
2023-10-01 14:29:36 +0300

    Revert "Added a button that changes text colour"
    This reverts commit e8d629545e8bb845dc4271648140612c3583b272.

--- app/src/main/java/com/example/labone_22/MainActivity.java
+++ app/src/main/java/com/example/labone_22/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.labone_22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
     Button btnChangeText;
-    Button btnChangeColour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         this.tvMain = findViewById(R.id.tvMain);
         this.btnChangeText = findViewById(R.id.btnChangeText);
-        this.btnChangeColour = findViewById(R.id.btnChangeColour);
 
         btnChangeText.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View view) {
                 tvMain.setText("Pasikeites tesktas!");
                 Toast.makeText(getApplicationContext(), "you did it!", Toast.LENGTH_LONG).show();
             }
         });
 
-        btnChangeColour.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                tvMain.setTextColor(Color.parseColor("#d20000"));
-                Toast.makeText(getApplicationContext(), "colour changed, yay!", Toast.LENGTH_LONG).show();
-            }
-        });
 
     }
 }
